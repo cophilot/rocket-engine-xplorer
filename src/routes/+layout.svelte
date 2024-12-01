78571d7d44982eee51c4df58ea1e2c7b67dcdf20
cophilot <philxsb@gmail.com>
2024-12-01 13:42:08 +0100

    update: Sun 01 Dec 2024 01:42:08 PM CET

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,10 @@
-<script>
+<script lang="ts">
 	import '../app.scss';
-	import ComparisonView from './ComparisonView.svelte';
-	import EngineOverview from './EngineOverview.svelte';
-	import logo from '../assets/logo.png';
-	let title = 'Rocket Engine Xplorer';
+	let { children } = $props();
 </script>
 
-<head>
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="description" content="Rocket Engine Xplorer" />
-	<meta name="keywords" content="rocket, engine, xplorer" />
-	<meta name="author" content="Philipp B." />
-	<link rel="icon" type="image/svg+xml" href={logo} />
-	<title>{title}</title>
-</head>
-
 <div class="app">
-	<img src={logo} alt="" class="logo" />
-	<h1>Rocket Engine Xplorer</h1>
-	<p class="info">
-		Welcome to the Rocket Engine Xplorer! This is a simple web app that allows you to explore and
-		compare the different rocket engines that have been used in space exploration.
-	</p>
-	<EngineOverview />
-	<ComparisonView></ComparisonView>
-	<p class="by info">
-		By
-		<a href="https://philipp-bonin.com/" target="_blank">Philipp B.</a>
-	</p>
+	{@render children()}
 </div>
 
 <style>
@@ -38,29 +13,5 @@
 		flex-direction: column;
 		align-items: center;
 		width: 100vw;
-	}
-	.by {
-		margin: 50px;
-	}
-
-	.logo {
-		margin-top: 20px;
-		width: 200px;
-		height: 200px;
-	}
-
-	@media (max-width: 600px) {
-		.info {
-			font-size: 0.8rem;
-		}
-	}
-	@media (max-width: 768px) {
-		.app {
-			width: 100vw;
-		}
-		.logo {
-			width: 100px;
-			height: 100px;
-		}
 	}
 </style>
---
+++ src/routes/engine/[name]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+	let name = $page.params.name;
+</script>
+
+<button>
+	{name}
+</button>
+
+<style>
+</style>
